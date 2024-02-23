919801cdf135cf31d287430ba47a01526acd5c16
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-02-24 02:02:22 +0330

    add

---
+++ 09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/springboot/demosecurity/DemosecurityApplication.java
@@ -0,0 +1,94 @@
+package com.luv2code.springboot.demosecurity;
+
+import com.luv2code.springboot.demosecurity.dao.AppDAO;
+import com.luv2code.springboot.demosecurity.entity.Instructor;
+import com.luv2code.springboot.demosecurity.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemosecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosecurityApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner ->{
+
+//			createInstructor(appDAO);
+//			findInstructor(appDAO);
+//			deleteInstructor(appDAO);
+//			findInstructorDetail(appDAO);
+//			deleteInstructorDetail(appDAO);
+		};
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		Long theId = 6L;
+		System.out.println("Deleting instructor detail id: " +theId);
+		appDAO.deleteInstructorDetailById(theId);
+		System.out.println("Done!");
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+
+		// get the instructor detail object
+		Long theId = 2L;
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+		// print the instance detail
+		System.out.println("TempInstructorDetail: " + tempInstructorDetail);
+
+		// print the associated instructor
+		System.out.println("The associated instructor: " + tempInstructorDetail.getInstructor());
+		System.out.println("Done!");
+
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		Long theId = 1L;
+
+		System.out.println("Deleting instructor id : " + theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		System.out.println("Finding instructor id: 2");
+		Instructor tempInstructor = appDAO.findInstructorById(2L);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only:" + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		// create the Instructor
+		Instructor tempInstructor =
+				new Instructor("Yazdan", "RafieeManesh", "yrafieemanesh@gmail.com");
+
+		// create the instructorDetail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"youtube",
+						"Programming"
+				);
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		// NOTE: this will ALSO save the details object
+		// because of CascadeTyp.All
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+		System.out.println("Done!");
+	}
+
+
+}
---
+++ 09-spring-boot-jpa-advanced-mapping/03-jpa-one-to-many/src/main/java/com/luv2code/springboot/demosecurity/entity/InstructorDetail.java
@@ -0,0 +1,88 @@
+package com.luv2code.springboot.demosecurity.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH,
+                                                        CascadeType.MERGE,
+                                                        CascadeType.PERSIST,
+                                                        CascadeType.REFRESH})
+    private Instructor instructor;
+
+    public InstructorDetail(){
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
